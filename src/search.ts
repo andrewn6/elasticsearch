d1ecb19d7f075c821be6b2e6f6ca49a8ec722ee4
andrew <andrewnijmeh1@gmail.com>
2021-10-27 20:35:05 -0400

    :bug: fix(string is not assignable to boolean for process.env

--- src/search.ts
+++ src/search.ts
@@ -11,27 +11,27 @@ const app = express();
 
 app.use(bodyParser.json({ limit: "15mb" }));
 app.use(bodyParser.urlencoded({
-	extended: true,
-	limit: "15mb"
+  extended: true,
+  limit: "15mb"
 }));
-app.use(cookieParser(process.env.sessionsecret));
+app.use(cookieParser(process.env.sessionsecret))
 
 if(process.env.isProduction) {
-	app.set("Trust proxy", 1);
+  app.set("Trust proxy", 1);
 }
 
 app.use(logging(
-	"elasticsearch",
-	"INFO",
-	process.env.isProduction,
+  "elasticsearch",
+  "INFO",
+  !process.env.isProduction,
 ));
 
 app.use("*", (req, res, next) => {
-	next(new ErrorHandler(404, "Route not Found!"));
+  next(new ErrorHandler(404, "Route not Found!"));
 });
 
 app.use((err: ErrorHandler, req: express.Request, res: express.Response, next: express.NextFunction) => {
-	return handleError(err, res);
+  return handleError(err, res);
 });
 
 const server = app.listen(process.env.PORT);
