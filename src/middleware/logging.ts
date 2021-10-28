cda9d5718c750c6af775f4e306359db61bfd49f1
andrew <andrewnijmeh1@gmail.com>
2021-10-27 20:32:25 -0400

    :bug: chore(add --fix flag to yarn lint)

--- src/middleware/logging.ts
+++ src/middleware/logging.ts
@@ -11,47 +11,47 @@ import * as fileSystem from "../lib/file";
 import { ErrorHandler } from "../lib/error";
 
 const transportCreator = (filename: string, dirname: string, level: string) => {
-  if (!filename) {
-    throw new ErrorHandler(500, "Internal Server Errror");
-  }
+	if (!filename) {
+		throw new ErrorHandler(500, "Internal Server Errror");
+	}
 
-  return new (DailyRotateFile)({
-    filename,
-    dirname,
-    datePattern: "YYYY-MM-DD",
-    level,
-    format: winston.format.combine(
-      winston.format.timestamp(),
-      winston.format.json()
-    )
-  });
+	return new (DailyRotateFile)({
+		filename,
+		dirname,
+		datePattern: "YYYY-MM-DD",
+		level,
+		format: winston.format.combine(
+			winston.format.timestamp(),
+			winston.format.json()
+		)
+	});
 };
 
 const createTransports = (filename: string, folder: string, level="INFO", consoleFlag=true) => {
-  const transports: ITransport[] = [];
+	const transports: ITransport[] = [];
 
-  if (consoleFlag) {
-    transports.push(new winston.transports.Console({
-      level,
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.simple(),
-        winston.format.timestamp()
-      )
-    }));
-  }
-  transports.push(transportCreator(filename, folder, level));
-  return transports;
+	if (consoleFlag) {
+		transports.push(new winston.transports.Console({
+			level,
+			format: winston.format.combine(
+				winston.format.colorize(),
+				winston.format.simple(),
+				winston.format.timestamp()
+			)
+		}));
+	}
+	transports.push(transportCreator(filename, folder, level));
+	return transports;
 };
 
 export default (filename: string, level="INFO", consoleFlag=true) => {
-  const folder = path.join(__dirname, "../../logs");
-  fileSystem.createDirectoryPath(folder);
-  const transports = createTransports(filename, folder, level, consoleFlag);
-  return expressWinston.logger({
-    transports,
-    meta: false,
-    expressFormat: true,
-    ignoreRoute: (req, res) => false
-  });
- };
+	const folder = path.join(__dirname, "../../logs");
+	fileSystem.createDirectoryPath(folder);
+	const transports = createTransports(filename, folder, level, consoleFlag);
+	return expressWinston.logger({
+		transports,
+		meta: false,
+		expressFormat: true,
+		ignoreRoute: (req, res) => false
+	});
+};
